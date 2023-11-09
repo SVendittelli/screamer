b7dd00c8a0e5acb772e4122cb29e4d7fc57ce177
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-11-09 17:10:01 +0000

    build: add bundle analyzer
    Add @next/bundle-analyzer to keep an eye on the bundle size

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withBundleAnalyzer({});
 
 module.exports = nextConfig;
