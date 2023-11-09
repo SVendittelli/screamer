c16021184f2da42ed6bb93ae1e08c55d959e19ab
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-11-09 19:07:10 +0000

    fix(next.js): add missing empty config for next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import { env } from "./env.mjs";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({ enabled: env.ANALYZE });
+const nextConfig = withBundleAnalyzer({ enabled: env.ANALYZE })({});
 
 export default nextConfig;
