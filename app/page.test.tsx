dfdc01509d2089482e86240ab27c2c46087e76d1
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-11-10 09:17:46 +0000

    feat(home): add logo
    Create a logo for the app and add it to the home screen and favicons.

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -16,19 +16,19 @@ describe("Home", () => {
 
   it("renders a 'Demo' button", () => {
     render(<Home />);
-    const demoButton = screen.getByText(/Demo/);
+    const demoButton = screen.getByText(/Try demo/);
     expect(demoButton).toBeInTheDocument();
   });
 
   it("renders a 'Sign Up' button", () => {
     render(<Home />);
-    const signUpButton = screen.getByText(/Sign Up/);
+    const signUpButton = screen.getByText(/Sign up/);
     expect(signUpButton).toBeInTheDocument();
   });
 
   it("renders a 'Login' button", () => {
     render(<Home />);
-    const loginButton = screen.getByText(/Login/);
+    const loginButton = screen.getByText(/Log in/);
     expect(loginButton).toBeInTheDocument();
   });
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
-import Link from "next/link";
 import { creepster } from "@/app/ui/fonts";
+import whiteLogo from "@/public/logo-white.min.svg";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
 
 const buttonClasses =
-  "bg-neutral-950 hover:bg-neutral-700 no-underline font-bold py-2 px-4 rounded-full mb-3";
+  "bg-neutral-950 hover:bg-neutral-700 no-underline font-bold py-2 px-4 rounded-full text-center";
 
 export default function Home() {
   return (
-    <main className="prose prose-neutral prose-invert flex min-h-screen flex-col items-center justify-center bg-red-700">
-      <h1 className={creepster.className}>Screamer</h1>
-      <Link href="/demo" role="button" className={buttonClasses}>
-        Demo
-      </Link>
-      <button className={buttonClasses}>Sign Up</button>
-      <button className={buttonClasses}>Login</button>
+    <main className="prose prose-neutral prose-invert min-h-screen flex flex-col gap-10 items-center justify-center bg-gradient-to-b from-red-800 to-red-600">
+      <header className="flex gap-2">
+        <Image className="not-prose" src={whiteLogo} height={36} alt="" />
+        <h1 className={clsx(creepster.className, "mb-0")}>Screamer</h1>
+      </header>
+      <nav className="flex flex-col gap-2">
+        <Link href="/demo" role="button" className={buttonClasses}>
+          Try demo
+        </Link>
+        <button className={buttonClasses}>Sign up</button>
+        <button className={buttonClasses}>Log in</button>
+      </nav>
     </main>
   );
 }
