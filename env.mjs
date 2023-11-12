d94ed2366a4fd45d09657814633b61046af41be4
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-11-12 22:01:23 +0000

    ci: run e2e tests on ci

--- env.mjs
+++ env.mjs
@@ -7,11 +7,18 @@ export const env = createEnv({
       .enum(["true", "false"])
       .optional()
       .transform((value) => value === "true"),
+    BASE_URL: z.string().optional(),
+    CI: z
+      .enum(["true", "false"])
+      .optional()
+      .transform((value) => value === "true"),
     PORT: z.coerce.number().default(3000),
   },
   client: {},
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
+    BASE_URL: process.env.BASE_URL,
+    CI: process.env.CI,
     PORT: process.env.PORT,
   },
 });
