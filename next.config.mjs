529ade9f66f3900f582cb6b81194c348ac3a11bd
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-11-24 00:08:59 +0000

    refactor: move redirects to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import { env } from "./env.mjs";
 
-const nextConfig = withBundleAnalyzer({ enabled: env.ANALYZE })({});
+const nextConfig = withBundleAnalyzer({ enabled: env.ANALYZE })({
+  async redirects() {
+    return [
+      {
+        source: "/view-source",
+        destination: "https://github.com/SVendittelli/screamer",
+        permanent: true,
+      },
+    ];
+  },
+});
 
 export default nextConfig;
