b991ae81a41ca6bec30055c1cb87ab485e16fb68
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-12-20 00:23:33 +0000

    feat(components): create ViewProfile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   // Toggle dark-mode based on .dark class
   darkMode: "class",
